c9e074215ac35d34416423dae4d4abee4ce1fd2f
RbCaViTerminal <robert@robertvail.info>
2024-02-07 21:45:26 -0500

    add get token from environment

--- gettoken.py
+++ gettoken.py
@@ -3,6 +3,13 @@ def gettoken():
 		from dotenv import dotenv_values
 		dotenv_values("cred/.env")['TOKEN']
 	except:
+		pass
+	try:
 		with open('creds/token') as f
 			token=f.read()
 		return token
+	except:
+		pass
+	try:
+		import os
+		return os.environ['token']
