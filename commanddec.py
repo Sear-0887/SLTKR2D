8eea8ed7e4018fe41c5a8db4163b596243f5ec9d
RbCaViTerminal <robert@robertvail.info>
2024-02-10 22:01:02 -0500

    commanddec.command2 has a classname parameter for commands where the class in lang is different from the command name

--- commanddec.py
+++ commanddec.py
@@ -16,16 +16,19 @@ def command(bot,name):
         return bot.command(name=name, description=getattr(cmds,name).desc, aliases=getattr(cmds,name).aliases)(trycmd(cmd))
     return fixcmd
 
-def command2(name):
+def command2(name,classname=None):
+    if classname is None:
+        classname=name
+    cmdclass=getattr(cmds,classname)
     async def _trycmd(cmd,ctx,*args,**kwargs):
         try:
             await cmd(ctx,*args,**kwargs)
         except Exception as e:
-            await ctx.send(getattr(cmds,name).error % args)
+            await ctx.send(cmdclass.error % args)
             print(e)
             raise e
     def trycmd(cmd):
         return decorator.decorate(cmd,_trycmd)
     def fixcmd(cmd):
-        return commands.command(name=name, description=getattr(cmds,name).desc, aliases=getattr(cmds,name).aliases)(trycmd(cmd))
+        return commands.command(name=name, description=cmdclass.desc, aliases=cmdclass.aliases)(trycmd(cmd))
     return fixcmd
