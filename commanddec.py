838a361fae1b4a59d7f09b52080ccb6fb7fe8e44
RbCaViTerminal <robert@robertvail.info>
2024-02-17 16:41:57 -0500

    merge the command decorators

--- commanddec.py
+++ commanddec.py
@@ -2,21 +2,7 @@ import decorator
 from lang import cmds
 from nextcord.ext import commands
 
-def command(bot,name):
-    async def _trycmd(cmd,ctx,*args,**kwargs):
-        try:
-            await cmd(ctx,*args,**kwargs)
-        except Exception as e:
-            await ctx.send(getattr(cmds,name).error.format(*args[1:]))
-            print(e)
-            raise e
-    def trycmd(cmd):
-        return decorator.decorate(cmd,_trycmd)
-    def fixcmd(cmd):
-        return bot.command(name=name, description=getattr(cmds,name).desc, aliases=getattr(cmds,name).aliases)(trycmd(cmd))
-    return fixcmd
-
-def command2(name,classname=None):
+def command(bot,name,classname=None):
     if classname is None:
         classname=name
     cmdclass=getattr(cmds,classname)
@@ -24,12 +10,14 @@ def command2(name,classname=None):
         try:
             await cmd(ctx,*args,**kwargs)
         except Exception as e:
-            print(cmdclass.error.format(*args[1:]))
             await ctx.send(cmdclass.error.format(*args[1:]))
             print(e)
             raise e
     def trycmd(cmd):
         return decorator.decorate(cmd,_trycmd)
     def fixcmd(cmd):
-        return commands.command(name=name, description=cmdclass.desc, aliases=cmdclass.aliases)(trycmd(cmd))
+        return bot.command(name=name, description=cmdclass.desc, aliases=cmdclass.aliases)(trycmd(cmd))
     return fixcmd
+
+def command2(name,classname=None):
+    return command(commands,name,classname)
