9e12fffbef44c208005e196c5342697978c59e73
RbCaViTerminal <robert@robertvail.info>
2024-02-18 23:05:47 -0500

    commands.command is actually different from bot.command

--- commanddec.py
+++ commanddec.py
@@ -10,8 +10,9 @@ def command(bot,name,classname=None):
         try:
             await cmd(ctx,*args,**kwargs)
         except Exception as e:
+            print('the thing:',e,type(e))
             await ctx.send(cmdclass.error.format(*args[1:]))
-            print(e)
+            print('the thing again:',e)
             raise e
     def trycmd(cmd):
         return decorator.decorate(cmd,_trycmd)
@@ -20,4 +21,20 @@ def command(bot,name,classname=None):
     return fixcmd
 
 def command2(name,classname=None):
-    return command(commands,name,classname)
+    if classname is None:
+        classname=name
+    cmdclass=getattr(cmds,classname)
+    async def _trycmd(cmd,self,ctx,*args,**kwargs):
+        try:
+            await cmd(ctx,self,*args,**kwargs)
+        except Exception as e:
+            print('the thing:',e,type(e))
+            print('the thing:',self,ctx,args,kwargs)
+            await ctx.send(cmdclass.error.format(*args[1:]))
+            print('the thing again:',e)
+            raise e
+    def trycmd(cmd):
+        return decorator.decorate(cmd,_trycmd)
+    def fixcmd(cmd):
+        return commands.command(name=name, description=cmdclass.desc, aliases=cmdclass.aliases)(trycmd(cmd))
+    return fixcmd
