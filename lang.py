e8316701d64eae4aebb32bb0f9852d9a64c083f5
RbCaViTerminal <robert@robertvail.info>
2024-02-25 15:06:35 -0500

    change the code in lang.py

--- lang.py
+++ lang.py
@@ -1,3 +1,7 @@
+import glob
+import re
+import collections
+
 keywords = {
     "Roody:2D Game Discord Server": {
         "link": "https://discord.gg/gbEkBNt",
@@ -18,11 +22,10 @@ linksstr="".join([
     for name,data in keywords.items()
 ])
 
+def recursiveddict():
+    return collections.defaultdict(recursiveddict)
 
-
-import glob
-import re
-cmdi = {}
+cmdi = recursiveddict()
 
 # delete 
 # def repri(): # USED FOR MIGRATING FROM CLASS METHOD TO .TXT FILES
@@ -33,23 +36,26 @@ cmdi = {}
 #                 if not cmdattr.startswith('__'):
 #                     print(f"{cmdname}.{cmdattr} = {getattr(clas, cmdattr)}")
 #             print("\n")
-            
+
 # ???
+
 def phraser():
-    for i in glob.glob("lang/en/*.txt"): # for each file in lang/en (use os.listdir)
-        with open(i , "r") as f: # open the file
-            fc = re.sub(r"\\\s*\n", r"\\", f.read()) # w h y
-            for line in fc.split("\n"): # read each line
-                if line.startswith("##"): continue # skip comments
-                for cmd, ele, val in re.findall(r"^(\w+)\.(\w+)\s*=\s*(.+)", line): # dot needs to be escaped # get word.word=text
-                    val = val.replace("\\", "\n") # undo the w h y
-                    if re.match(r"^\[.*\]$", val): # if val is bracketed: 
-                        val = val[1:-1].split(", ") # convert it to a list
-                        if val == ['']: val = [] # without empty elements
-                    try: cmdi[cmd] # use collections.defaultdict
-                    except: cmdi[cmd] = {} # seriously
-                    cmdi[cmd][ele] = val
-    print(cmdi["help"]["aliases"])
+    for fname in os.listdir(config.cmdlocaledir):
+        with open(fname) as f:
+            linesiter=iter(f)
+            for line in linesiter:
+                while line.endswith('\\\n'):
+                    line=line[:-2].strip()+'\n'+next(linesiter) # add the next line to this if this line ends with a backslash
+                line=re.sub('#.*$','',line) # remove comments
+                if '=' not in line:
+                    continue
+                key,value=line.split('=',maxsplit=1)
+                key=tuple(key.split('.'))
+                target=cmdi
+                for k in key[:-1]:
+                    target=target[k]
+                target[k[-1]]=value.strip()
+    #print(cmdi["help"]["aliases"])
     # EXCEPTIONS
     cmdi["link"]["desc"] = cmdi["link"]["desc"].format(linksstr) # aaaaaaaaaaaaaaaaaaaaaaaaaa
 
