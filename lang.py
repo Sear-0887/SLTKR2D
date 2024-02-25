2ece20cddef36ecb9ac7a2ac65d394ce42ff100f
RbCaViTerminal <robert@robertvail.info>
2024-02-25 14:02:44 -0500

    comments hahaha

--- lang.py
+++ lang.py
@@ -24,6 +24,7 @@ import glob
 import re
 cmdi = {}
 
+# delete 
 # def repri(): # USED FOR MIGRATING FROM CLASS METHOD TO .TXT FILES
 #     for cmdname in dir(cmds): 
 #         if not cmdname.startswith('__'):
@@ -32,27 +33,28 @@ cmdi = {}
 #                 if not cmdattr.startswith('__'):
 #                     print(f"{cmdname}.{cmdattr} = {getattr(clas, cmdattr)}")
 #             print("\n")
-                    
+            
+# ???
 def phraser():
-    for i in glob.glob("lang/en/*.txt"):
-        with open(i , "r") as f:
-            fc = re.sub(r"\\\s*\n", r"\\", f.read())
-            for line in fc.split("\n"):
-                if line.startswith("##"): continue
-                for cmd, ele, val in re.findall(r"^(\w+).(\w+)\s*=\s*(.+)", line):
-                    val = val.replace("\\", "\n")
-                    if re.match(r"^\[.*\]$", val):
-                        val = val[1:-1].split(", ")
-                        if val == ['']: val = []
-                    try: cmdi[cmd]
-                    except: cmdi[cmd] = {}
+    for i in glob.glob("lang/en/*.txt"): # for each file in lang/en (use os.listdir)
+        with open(i , "r") as f: # open the file
+            fc = re.sub(r"\\\s*\n", r"\\", f.read()) # w h y
+            for line in fc.split("\n"): # read each line
+                if line.startswith("##"): continue # skip comments
+                for cmd, ele, val in re.findall(r"^(\w+)\.(\w+)\s*=\s*(.+)", line): # dot needs to be escaped # get word.word=text
+                    val = val.replace("\\", "\n") # undo the w h y
+                    if re.match(r"^\[.*\]$", val): # if val is bracketed: 
+                        val = val[1:-1].split(", ") # convert it to a list
+                        if val == ['']: val = [] # without empty elements
+                    try: cmdi[cmd] # use collections.defaultdict
+                    except: cmdi[cmd] = {} # seriously
                     cmdi[cmd][ele] = val
     print(cmdi["help"]["aliases"])
     # EXCEPTIONS
-    cmdi["link"]["desc"] = cmdi["link"]["desc"].format(linksstr)
+    cmdi["link"]["desc"] = cmdi["link"]["desc"].format(linksstr) # aaaaaaaaaaaaaaaaaaaaaaaaaa
 
 def evl(target):
-    target = target.split(".")
+    target = target.split(".") # why this when cmdi always has two levels
     root = cmdi
     for i in target:
         root = root[i]
