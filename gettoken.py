326bcd535c6d62bb5afa3f96960a3b898e54f1e6
RbCaViTerminal <robert@robertvail.info>
2024-02-25 21:26:49 -0500

    fix gettoken

--- gettoken.py
+++ gettoken.py
@@ -1,22 +1,29 @@
 import config
+import os
 
 def gettoken():
+	errs=[]
 	try:
 		from dotenv import dotenv_values
-		print("test")
 		return dotenv_values(os.path.join(config.creddir,".env"))['TOKEN']
-	except:
-		pass
+	except Exception as e:
+		errs.append(e)
 	try:
 		with open(os.path.join(config.creddir,"token")) as f:
 			token=f.read()
 		return token
-	except:
-		pass
+	except Exception as e:
+		errs.append(e)
 	try:
-		import os
+		with open("token") as f:
+			token=f.read()
+		return token
+	except Exception as e:
+		errs.append(e)
+	try:
 		return os.environ['token']
-	except:
-		print("Error on Loading TOKEN.")
+	except Exception as e:
+		errs.append(e)
+	raise Exception("Error on Loading TOKEN.",errs)
 
 token=gettoken()
