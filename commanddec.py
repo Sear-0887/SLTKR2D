4cc93ae5972e62a7cd9b3bc6472556076b9424db
RbCaViTerminal <robert@robertvail.info>
2024-02-26 10:18:49 -0500

    remove commanddec.CogGroup

--- commanddec.py
+++ commanddec.py
@@ -49,28 +49,3 @@ def CogCommand(name):
             aliases     = evl(f"{name}.aliases")
         )( trycmd(cmd) )
     return fixcmd
-
-def CogGroup(name):
-    cmdclass = cmdi[name]
-    async def _trycmd(cmd,ctx,*args,**kwargs):
-        try:
-            await cmd(ctx,*args,**kwargs)
-        except Exception as e:
-            print('the thing:',e,type(e))
-            print('the thing:',ctx,args,kwargs)
-            try:
-                s=evl(f"{cmdclass}.error").format(*args,**kwargs)
-            except:
-                s=evl(f"{cmdclass}.error")
-            await ctx.send(s)
-            print('the thing again:',e)
-            raise e
-    def trycmd(cmd):
-        return decorator.decorate(cmd,_trycmd)
-    def fixcmd(cmd):
-        return commands.group(
-            name        =        name, 
-            description = evl(f"{cmdclass}.desc"), 
-            aliases     = evl(f"{cmdclass}.aliases")
-        )( trycmd(cmd) )
-    return fixcmd
