cc04b70e9bb7ca098675acff35e9b4821f4af1fc
Sear <kaheihei2020@gmail.com>
2024-02-26 12:59:56 +0800

    Fixed Decorator Error

--- commanddec.py
+++ commanddec.py
@@ -3,7 +3,6 @@ from lang import cmdi, evl
 from nextcord.ext import commands
 
 def MainCommand(bot,name):
-    cmdclass = cmdi[name]
     async def _trycmd(cmd,ctx,*args,**kwargs):
         try:
             await cmd(ctx,*args,**kwargs)
@@ -11,25 +10,23 @@ def MainCommand(bot,name):
             print('the thing:',e,type(e))
             print('the thing:',ctx,args,kwargs)
             try:
-                s=evl(f"{cmdclass}.error").format(*args,**kwargs)
+                s=evl(f"{name}.error").format(*args,**kwargs)
             except:
-                s=evl(f"{cmdclass}.error")
+                s=evl(f"{name}.error")
             await ctx.send(s)
             print('the thing again:',e)
             raise e
     def trycmd(cmd):
         return decorator.decorate(cmd,_trycmd)
     def fixcmd(cmd):
-        print(evl(f"{cmdclass}.aliases"))
         return bot.command(
             name        =        name, 
-            description = evl(f"{cmdclass}.desc"), 
-            aliases     = evl(f"{cmdclass}.aliases")
+            description = evl(f"{name}.desc"), 
+            aliases     = evl(f"{name}.aliases")
         )( trycmd(cmd) )
     return fixcmd
 
 def CogCommand(name):
-    cmdclass = cmdi[name]
     async def _trycmd(cmd,ctx,*args,**kwargs):
         try:
             await cmd(ctx,*args,**kwargs)
@@ -37,9 +34,9 @@ def CogCommand(name):
             print('the thing:',e,type(e))
             print('the thing:',ctx,args,kwargs)
             try:
-                s=evl(f"{cmdclass}.error").format(*args,**kwargs)
+                s=evl(f"{name}.error").format(*args,**kwargs)
             except:
-                s=evl(f"{cmdclass}.error")
+                s=evl(f"{name}.error")
             await ctx.send(s)
             print('the thing again:',e)
             raise e
@@ -48,8 +45,8 @@ def CogCommand(name):
     def fixcmd(cmd):
         return commands.command(
             name        =        name, 
-            description = evl(f"{cmdclass}.desc"), 
-            aliases     = evl(f"{cmdclass}.aliases")
+            description = evl(f"{name}.desc"), 
+            aliases     = evl(f"{name}.aliases")
         )( trycmd(cmd) )
     return fixcmd
 
