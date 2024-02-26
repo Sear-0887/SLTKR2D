052bdcb85d1546db3febf822a6a54ae4cbc5cb9b
RbCaViTerminal <robert@robertvail.info>
2024-02-26 09:18:13 -0500

    add inspect to viewcog to get command source files

--- cog_admin.py
+++ cog_admin.py
@@ -2,6 +2,7 @@ import glob
 import nextcord
 from nextcord.ext import commands
 from commanddec import command2
+import inspect
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -12,9 +13,15 @@ class Admin(commands.Cog):
     async def viewcog(self,ctx):
         embed = nextcord.Embed()
         embed.description = ""
+        print([o.__dict__ for i, o in self.bot.cogs.items()])
+        print([o.__cog_commands__ for i, o in self.bot.cogs.items()])
+        print([o.__cog_commands__[0].name for i, o in self.bot.cogs.items()])
+        print([type(o.__cog_commands__[0]) for i, o in self.bot.cogs.items()])
+        print([type(o.__cog_commands__[0]._callback) for i, o in self.bot.cogs.items()])
+        print([inspect.getfile(o.__cog_commands__[0]._callback) for i, o in self.bot.cogs.items()])
+        print({i:{c.name:inspect.getfile(c._callback.__wrapped__) for c in o.__cog_commands__} for i, o in self.bot.cogs.items()})
+        print([o.__cog_name__ for i, o in self.bot.cogs.items()])
         for cog_name in glob.glob("cog_*.py"):
-            print([dir(o) for i, o in self.bot.cogs.items()])
-            print([o.__cog_name__ for i, o in self.bot.cogs.items()])
             embed.description += f"| {cog_name[:-3]} \n"
         await ctx.send(embed=embed)
 
