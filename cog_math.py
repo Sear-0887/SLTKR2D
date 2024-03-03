e835739a70c575ed83a40250dd0f4d4c1734b115
RbCaViTerminal <robert@robertvail.info>
2024-03-03 14:16:53 -0500

    add !prime and !plot

--- cog_math.py
+++ cog_math.py
@@ -1,6 +1,9 @@
 from assetload import blockinfos, idtoblock as quickidtable,locale
+import matplotlib.pyplot as plt
+import numpy
 import nextcord
 import random
+import math
 import re
 from PIL import Image
 from nextcord.ext import commands
@@ -49,30 +52,54 @@ class Math(commands.Cog):
             formu = dif(formu)
             
         await ctx.send(f"{formulae} = {formu}")   
-            
-        # global formu
-        # formu = formulae
-        # def dec(inp, formu):
-        #     print(inp)
-        #     if inp == []: return formu
-        #     f, o, s = inp[0]
-        #     f, s = int(f), int(s)
-        #     result = None
-        #     if o == "*": result = f * s
-        #     if o == "/": result = f / s
-        #     if o == "+": result = f + s
-        #     if o == "-": result = f - s
-        #     if result != None:
-        #         replaced = formu.replace(str(f)+o+str(s), str(result))
-        #         dec(re.findall(rf"(\d+)(\{o})(\d+)", replaced), replaced)
-        #         return 
-        #     return formu
-        # formu = formu.replace(" ", "")
-        # formu = dec(re.findall(r"(\d+)(\*)(\d+)", formu), formu)
-        # formu = dec(re.findall(r"(\d+)(\/)(\d+)", formu), formu)
-        # formu = dec(re.findall(r"(\d+)(\+)(\d+)", formu), formu)
-        # formu = dec(re.findall(r"(\d+)(\-)(\d+)", formu), formu)
+    
+    @CogCommand("plot")
+    async def plot(self, ctx:commands.Context, slope:int=3, yinter:int=3, min_:int=-20, max_:int=20):
+        xaxis = numpy.arange(min_, max_, 0.1)
+        yaxis = []
+        for x in xaxis:
+            yaxis += [slope*x+yinter]
+        plt.plot(xaxis, yaxis)
+        showy = f"{slope}x"
+        if yinter != 0: showy += ["+",""][yinter<0] + str(yinter)
+        plt.savefig("cache/plot.png")
+        plt.grid()
         
+        await ctx.send(file=nextcord.File("cache/plot.png", filename=f"{showy}.png"))
+        plt.close()
+    
+    # Copied from my old code, should run N (2≤N≤10^9) within 1 sec
+    @CogCommand("prime")
+    async def prime(self, ctx:commands.Context, n:int=12):
+        if n < 0: raise Exception('Negetive Value')
+        r = n
+        i = 2
+        c = []
+        while i*i<=n:
+            if n % i == 0:
+                n //= i
+                c.append(str(i))
+            else:
+                i += 1
+
+        if n >= 1:
+            c.append(str(i))
+        await ctx.send(f"{r} = {' * '.join(c)}")
+    # Near Same tested as !prime 
+    @CogCommand("factor")
+    async def factor(self, ctx:commands.Context, n:int=12):
+        r = n
+        m = int(math.sqrt(n))
+        factor = []
+        for i in range(1, m + 1):
+            if n % i == 0:
+                factor.append(str(i))
+        if m * m == n:
+            m -= 1
+        for i in range(m, 0, -1):
+            if n % i == 0:
+                factor.append(str(n // i))
+        await ctx.send(f"{r} has {len(factor)} factors: \n{', '.join(factor)}")
         
 def setup(bot):
 	bot.add_cog(Math(bot))
