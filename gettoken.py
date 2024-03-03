498ac55f234dad60de0a0e6e75581c74a3bf37bc
RbCaViTerminal <robert@robertvail.info>
2024-03-03 12:41:36 -0500

    add methods of getting the token (optional)

--- gettoken.py
+++ gettoken.py
@@ -1,8 +1,29 @@
+import config
+import os
+
 def gettoken():
+	errs=[]
 	try:
 		from dotenv import dotenv_values
-		print("test")
-		return dotenv_values("cred/.env")['TOKEN']
-		
-	except:
-		print("Error on Loading TOKEN.")
+		return dotenv_values(os.path.join(config.creddir,".env"))['TOKEN']
+	except Exception as e:
+		errs.append(e)
+	try:
+		with open(os.path.join(config.creddir,"token")) as f:
+			token=f.read()
+		return token
+	except Exception as e:
+		errs.append(e)
+	try:
+		with open("token") as f:
+			token=f.read()
+		return token
+	except Exception as e:
+		errs.append(e)
+	try:
+		return os.environ['token']
+	except Exception as e:
+		errs.append(e)
+	raise Exception("Error on Loading TOKEN.",errs)
+
+token=gettoken()
