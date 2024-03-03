162eea4f33a58b2f55d0b3b768126de6249a998c
RbCaViTerminal <robert@robertvail.info>
2024-03-03 14:16:08 -0500

    change help

--- main.py
+++ main.py
@@ -31,31 +31,40 @@ async def reloadlocal(ctx):
 
 @MainCommand(bot,"help")
 async def help(ctx, cmdname=None,):
+    async def gethelplist(interaction):
+        sembed = nextcord.Embed()
+        sembed.title = "Help List"
+        sembed.description = "Here's a list of commands:\n" + ", ".join(cmdi.keys())
+        await interaction.send(ephemeral=True, embed=sembed)
+    async def gethelplist(interaction:nextcord.Interaction, helplist:list|str=[]):
+        author = interaction.user
+        preparedlist = []
+        for key in bot.commands:
+            if key.name in helplist or helplist == []:
+                preparedlist.append(f"### {key.name} ({'/'.join(key.aliases)})")
+                preparedlist.append(f"{key.description}")
+        sembed = nextcord.Embed()
+        sembed.title = evl("help","helplist","title")
+        sembed.description = evl("help","helplist","desc").format("\n".join(preparedlist))
+        
+        await interaction.send(ephemeral=True, embed=sembed)
     if not cmdname:
         embed = nextcord.Embed()
         embed.description = cmdi["help"]["blankdisplay"].format(datetime.datetime.now()-TimeOn)
         view = nextcord.ui.View()
-        async def gethelplist(interaction):
-            sembed = nextcord.Embed()
-            sembed.title = "Help List"
-            sembed.description = "Here's a list of commands:\n" + ", ".join(cmdi.keys())
-            await interaction.send(ephemeral=True, embed=sembed)
         getlistbtn = nextcord.ui.Button(style=nextcord.ButtonStyle.blurple, label="Help List")
         getlistbtn.callback = gethelplist
         view.add_item(getlistbtn)
         await ctx.send(embed=embed, view=view)
     else:
-        for i in bot.all_commands:
-            if cmdname in bot.all_commands[i].aliases:
-                cmdname = i
-            if cmdname == i:
+        for cmd in bot.commands:
+            if cmdname in cmd.aliases or cmdname == cmd.name:
                 embed = nextcord.Embed()
-                embed.title = cmdname
-                embed.description = cmdi[cmdname]["desc"]
-                embed.add_field(name="Syntax", value=cmdi[cmdname]["syntax"])
-                embed.add_field(name="Aliases", value=",\n".join(cmdi[cmdname]["aliases"]))
-                await ctx.send(embed=embed)
-                return
+                embed.title = cmd.name
+                embed.description = evl(cmd.name,"desc")
+                embed.add_field(name="Syntax", value=evl(cmd.name,"syntax"))
+                embed.add_field(name="Aliases", value=",\n".join(cmd.aliases))
+                embed.add_field(name="Cog", value='Main' if cmd.cog_name == None else cmd.cog_name)
         else:
             raise Exception('') # the decorator will handle it
 
