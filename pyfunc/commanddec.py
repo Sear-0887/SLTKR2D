e8efbd05375aaa4b5d1f3e5434a2c135d0f58fa6
RbCaViTerminal <robert@robertvail.info>
2024-03-08 15:15:03 -0500

    add a separator between error reports

--- pyfunc/commanddec.py
+++ pyfunc/commanddec.py
@@ -46,7 +46,7 @@ f'''
         kwargsstr = '\n'.join([k+':'+s for i,(k,v) in enumerate(kwargs.items()) for s in repr(v).split('\n')])
         exctbstr = '\n'.join(['exc-:'+s for s in '\n'.join(traceback.format_exception(e)).split('\n')])
         fil.write('\n'.join([userstr,timestr,cmdstr,argsstr,kwargsstr,exctbstr]))
-        fil.write('\n')
+        fil.write('\n####:####\n') # record separator
     # raise e
 
 def MainCommand(bot,name):
