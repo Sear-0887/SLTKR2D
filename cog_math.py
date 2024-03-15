36a9af134e8d646f4cb0aba3605ef12f484e101b
Sear <kaheihei2020@gmail.com>
2024-03-15 22:37:17 +0800

    moved primefactor() of !prime and !factor to a seperate math.py

--- cog_math.py
+++ cog_math.py
@@ -7,36 +7,14 @@ import math
 import re
 import time
 from PIL import Image
-from collections import defaultdict
+from pyfunc.math import primefactor
 from nextcord.ext import commands
 from pyfunc.lang import lprint
 from pyfunc.commanddec import CogCommand
 
-# rbcavi implementation
-smallprimes=[2,3,5,7,11,13,17,19] # i hope i didn't miss any
-# you can add more primes
-# just don't skip any
 
-def primefactor(n):
-    if n==1:
-        return {1:1}
-    factors=defaultdict(int) # a dict of primes to powers, all 0
-    for prime in smallprimes:
-        while n%prime==0: # assumes 1 isn't in the list
-            n//=prime
-            factors[prime]+=1
-    p=max(smallprimes) # assumes the maximum prime is odd
-    while n>1:
-        while n%p==0:
-            n//=p
-            factors[p]+=1
-        p+=2 # 100% faster!
-        if p*p>n: # sqrt is a bit slower
-            # n is now prime
-            if n!=1:
-                factors[n]+=1
-            break
-    return factors
+
+
 
 class Math(commands.Cog):
     def __init__(self, bot):
---
+++ pyfunc/math.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+def primefactor(n):
+    # rbcavi implementation
+    smallprimes=[2,3,5,7,11,13,17,19] # i hope i didn't miss any
+    # you can add more primes
+    # just don't skip any
+    if n==1:
+        return {1:1}
+    factors=defaultdict(int) # a dict of primes to powers, all 0
+    for prime in smallprimes:
+        while n%prime==0: # assumes 1 isn't in the list
+            n//=prime
+            factors[prime]+=1
+    p=max(smallprimes) # assumes the maximum prime is odd
+    while n>1:
+        while n%p==0:
+            n//=p
+            factors[p]+=1
+        p+=2 # 100% faster!
+        if p*p>n: # sqrt is a bit slower
+            # n is now prime
+            if n!=1:
+                factors[n]+=1
+            break
+    return factors
