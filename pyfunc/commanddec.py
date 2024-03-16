06453180a14ddc285a8ef0ed0a09f4ea06a9163e
RbCaViTerminal <robert@robertvail.info>
2024-03-16 11:49:42 -0400

    revert

--- pyfunc/commanddec.py
+++ pyfunc/commanddec.py
@@ -1,6 +1,5 @@
 import datetime
 import decorator
-import nextcord
 from pyfunc.lang import cmdi, evl
 from colorama import Fore, init
 from nextcord.ext import commands
@@ -39,22 +38,16 @@ f'''
     )
     await ctx.send(expecterr)
     with open(f"cache/log/error-{ctx.author.global_name}-{datetime.date.today():%d-%m-%Y}.txt", "a+") as fil:
-        trigger = '\n'.join([f'Trigger Command: {s}' for s in ctx.message.clean_content.split('\n')])
-        arg = '\n'.join([f'Arg {i.zfill(2)}:'+s for i,arg in enumerate(args) for s in repr(arg).split('\n')])
-        kwarg = '\n'.join([f'Kwarg "{k}": {s}' for _,(k,v) in enumerate(kwargs.items()) for s in repr(v).split('\n')])
-        errname = ', '.join([f'{s}' for s in str(e).split('\n')])
-        errline = '\n'.join([f'| {s}' for s in '\n'.join(traceback.format_exception(e)).split('\n')])
-        wholestr = f'''
-User: {ctx.author.display_name}/{ctx.author.global_name} (<@{ctx.author.id}> in Server {ctx.guild.name})
-At: {datetime.datetime.now().isoformat()}'
-{trigger}
-{arg}
-{kwarg}
-ExceptionName: " {errname} "
-Detail:
-{errline}
-        '''
-        fil.write(wholestr)
+        userstr = f'user-:{ctx.author.display_name}\n'
+        userstr += f'user-:{ctx.author.global_name}\n'
+        userstr += f'user-:{ctx.author.id}'
+        timestr = f'time-:{datetime.datetime.now().isoformat()}'
+        cmdstr = '\n'.join([f'cmd-:{s}' for s in ctx.message.clean_content.split('\n')])
+        argsstr = '\n'.join([f'arg-{i}:'+s for i,arg in enumerate(args) for s in repr(arg).split('\n')])
+        kwargsstr = '\n'.join([k+':'+s for i,(k,v) in enumerate(kwargs.items()) for s in repr(v).split('\n')])
+        exctbstr = '\n'.join(['exctb-:'+s for s in '\n'.join(traceback.format_exception(e)).split('\n')])
+        excstr = '\n'.join([f'exc-:{s}' for s in str(e).split('\n')])
+        fil.write('\n'.join([userstr,timestr,cmdstr,argsstr,kwargsstr,exctbstr,excstr]))
         fil.write('\n####:####\n') # record separator
     # raise e
 
