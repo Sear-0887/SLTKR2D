13b2c5624bee11f63f030f547771d4366e09ee74
RbCaViTerminal <robert@robertvail.info>
2024-04-24 13:07:01 -0400

    test rotation

--- testblock.py
+++ testblock.py
@@ -3,3 +3,12 @@ from pyfunc.block import makeimage
 im = makeimage([['iron_bar']])
 
 im.show()
+
+im = makeimage([[
+	{'type':'actuator_head','rotate':0},
+	{'type':'actuator_head','rotate':1},
+	{'type':'actuator_head','rotate':2},
+	{'type':'actuator_head','rotate':3},
+]])
+
+im.show()
