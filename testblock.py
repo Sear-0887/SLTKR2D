02eca87ba81fcf9bae133387d6f6531b5cbd5796
RbCaViTerminal <robert@robertvail.info>
2024-04-24 15:41:02 -0400

    merge the test cases

--- testblock.py
+++ testblock.py
@@ -1,23 +1,21 @@
 from pyfunc.block import makeimage
 
-im = makeimage([['iron_bar']])
-
-im.show()
-
-im = makeimage([[
-	{'type':'actuator_head','rotate':0},
-	{'type':'actuator_head','rotate':1},
-	{'type':'actuator_head','rotate':2},
-	{'type':'actuator_head','rotate':3},
-]])
-
-im.show()
-
-im = makeimage([[
-	{'type':'frame','rotate':0},
-	{'type':'frame','rotate':1},
-	{'type':'frame','rotate':2},
-	{'type':'actuator_head','rotate':3},
-]])
+im = makeimage([
+	['iron_bar','air',{'type':'iron_bar','rotate':1}],
+	[],
+	[
+		{'type':'actuator_head','rotate':0},
+		{'type':'actuator_head','rotate':1},
+		{'type':'actuator_head','rotate':2},
+		{'type':'actuator_head','rotate':3},
+	],
+	[],
+	[
+		{'type':'frame','rotate':0},
+		{'type':'frame','rotate':1},
+		{'type':'frame','rotate':2},
+		{'type':'actuator_head','rotate':3},
+	]
+])
 
 im.show()
