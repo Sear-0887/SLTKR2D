b70389982741b7406384dc6d8845b38feddf2c24
RbCaViTerminal <robert@robertvail.info>
2024-04-27 22:14:11 -0400

    remove the old data processing code

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ data=collections.defaultdict(list)
 for x in rawdata:
     x=copy.deepcopy(x)
     typ=x['type']
-    del x['type']
     data[typ].append(x)
 
 print(json.dumps({dk:[*set([k for x in d for k in x.keys()])] for dk,d in data.items()},indent=2))
@@ -37,65 +36,6 @@ def handletags(s:str) -> str | list:
         print(f'{s} is Just a Normal name starts with $ ???????')
     return s
 
-heat=collections.defaultdict(list)
-
-for h in data['heat']:
-    h=copy.deepcopy(h)
-    h['needs_entity'] = 'needs_entity' in h and h['needs_entity']=='1'
-    product = h['product']
-    del h['product']
-    heat[product].append(h)
-
-extract=collections.defaultdict(list)
-
-for e in data['extract']:
-    e=copy.deepcopy(e)
-    extract[e['product']].append(e)
-
-inject=collections.defaultdict(list)
-
-for i in data['inject']:
-    i=copy.deepcopy(i)
-    if 'transform_receiver' in i['product']:
-        inject[i['product']['transform_receiver']].append(i)
-    else:
-        inject['_fertilize'].append(i)
-
-combine=collections.defaultdict(list)
-
-for c in data['combine']:
-    c=copy.deepcopy(c)
-    product=c['product']
-    del c['product']
-    c['product']=product['block']
-    c['amount']=product['amount']
-    combine[c['product']].append(c)
-
-extra_display=collections.defaultdict(list)
-
-for e in data['extra_display']: # i don't want to deal with this now
-    e=copy.deepcopy(e)
-    product=e['product']
-    del e['product']
-    products = handletags(product['filter'])
-    if isinstance(products,str):
-        products = [products]
-    for p in products:
-        e2=copy.deepcopy(e)
-        e2['product']=p
-        e2['amount']=product.get('amount',1)
-        extra_display[e2['product']].append(e2)
-
-summonore_pill=set()
-
-for s in data['summonore_pill']: # there is literally only one
-    s=copy.deepcopy(s)
-    products = handletags(s['filter'])
-    if isinstance(products,str):
-        products = [products]
-    for p in products:
-        summonore_pill.add(p)
-
 # filters:
 # grassy
 # non_air
@@ -106,52 +46,6 @@ for s in data['summonore_pill']: # there is literally only one
 
 import schema
 
-tagschema=schema.Schema({str:[str]})
-tags = tagschema.validate(tags)
-
-heatschema=schema.Schema({str:[{
-    'ingredient':str,
-    'time':schema.Use(int),
-    'needs_entity':bool,
-    schema.Optional('surrounding'):{
-        'block':str,
-        'minimum':schema.Use(int),
-    },
-}]})
-heat = heatschema.validate(heat)
-
-extractschema=schema.Schema({str:[{
-    'product':str,
-    'amount':schema.Use(int),
-    'ingredient':str,
-    'time':schema.Use(int),
-    schema.Optional('post_action'):schema.Or('destroy','reduce_container_count5')
-}]})
-extract = extractschema.validate(extract)
-
-injectschema=schema.Schema({str:[{
-    'product':schema.Or(
-        {
-            'transform_receiver':str
-        },
-        {
-            'fertilizer':schema.Use(int)
-        }
-    ),
-    'pill':str,
-    'receiver':str,
-    schema.Optional('needs_passive'):str
-}]})
-inject = injectschema.validate(inject)
-
-combineschema=schema.Schema({str:[{
-    'grid':[[schema.Use(handletags)]],
-    'product':str,
-    'amount':schema.Use(int),
-    schema.Optional('needs_passive'):str
-}]})
-combine = combineschema.validate(combine)
-
 def fixemptygrid(g):
     if g == '':
         return [[]]
@@ -161,28 +55,6 @@ def fixemptygrid(g):
 def handlepos(p):
     x,y = p.split(',')
     return (int(x),int(y))
-
-extra_displayschema=schema.Schema({str:[{
-    'grid':schema.Or(schema.Use(fixemptygrid),[[schema.Use(handletags)]]),
-    'product':str,
-    'amount':schema.Use(int),
-    schema.Optional('arrow_sprite'):schema.Use(int),
-    schema.Optional('guidebook_page_blacklist'):[str],
-    schema.Optional('guidebook_page_whitelist'):[str],
-    schema.Optional('weldall'):schema.Use(int),
-    schema.Optional('needs_passive'):str,
-    schema.Optional('research_requirement_override'):str,
-    schema.Optional('entity'):{
-        'type':str,
-        'position':schema.Use(handlepos)
-    },
-    schema.Optional('guidebook_use_only'):schema.Use(int),
-    schema.Optional('match_filter_modulo'):schema.Use(int),
-}]})
-extra_display = extra_displayschema.validate(extra_display)
-
-summonore_pillschema=schema.Schema({str})
-summonore_pill = summonore_pillschema.validate(summonore_pill)
 
 def strtobool(s):
     assert s in ['0','1']
@@ -235,10 +107,12 @@ flag = schema.Use(strtobool)
 
 dataschema = schema.Schema({
     'tag':[{
+        'type':'tag',
         'name':str,
         'blocks':[block]
     }],
     'heat':[{
+        'type':'heat',
         'product':block,
         'ingredient':blocktag,
         'time':num,
@@ -249,6 +123,7 @@ dataschema = schema.Schema({
         },
     }],
     'extract':[{
+        'type':'extract',
         'product':block,
         'amount':num,
         'ingredient':blocktag,
@@ -256,6 +131,7 @@ dataschema = schema.Schema({
         schema.Optional('post_action'):schema.Or('destroy','reduce_container_count5')
     }],
     'inject':[{
+        'type':'inject',
         'product':schema.Or(
             {
                 'transform_receiver':block
@@ -269,6 +145,7 @@ dataschema = schema.Schema({
         schema.Optional('needs_passive'):research
     }],
     'combine':[{
+        'type':'combine',
         'grid':[[blocktag]],
         'product':{
             'block':block,
@@ -277,6 +154,7 @@ dataschema = schema.Schema({
         schema.Optional('needs_passive'):research
     }],
     'extra_display':[{
+        'type':'extra_display',
         'grid':schema.Or(schema.Use(fixemptygrid),[[blocktag]]),
         'product':{
             'filter':blocktag,
@@ -295,9 +173,18 @@ dataschema = schema.Schema({
         schema.Optional('guidebook_use_only',default = False):flag,
         schema.Optional('match_filter_modulo'):flag, # the changing blocks all match
     }],
-    'summonore_pill':[{'filter':blocktag}],
-    'sensor_natural':[{'filter':blocktag}],
-    'sensor_rare_resource':[{'filter':blocktag}],
+    'summonore_pill':[{
+        'type':'summonore_pill',
+        'filter':blocktag
+    }],
+    'sensor_natural':[{
+        'type':'sensor_natural',
+        'filter':blocktag
+    }],
+    'sensor_rare_resource':[{
+        'type':'sensor_rare_resource',
+        'filter':blocktag
+    }],
 })
 
 data2=dataschema.validate(data)
